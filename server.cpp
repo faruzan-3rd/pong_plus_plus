4b3859f378e5594d98a5901f6f730102765853dd
wasabi3D <wasabi3D@users.noreply.github.com>
2022-12-21 14:34:18 +0100

    Rewrote server code for 2 players (Not tested yet)

--- server.cpp
+++ server.cpp
@@ -5,6 +5,9 @@
 #include <string>
 
 
+int assign_player(sf::TcpSocket& player1, sf::TcpSocket& player2, sf::TcpListener& listener);
+
+
 int main(){
     std::ifstream ifs;
     std::string line;
@@ -23,33 +26,73 @@ int main(){
     ip = input[0]; port = std::stoi(input[1]);
 
     sf::TcpListener listener;
-    listener.setBlocking(true);
+    listener.setBlocking(false);
+    sf::TcpSocket player1, player2;
+    player1.setBlocking(false);
+    player2.setBlocking(false);
 
-    if(listener.listen(port) != sf::Socket::Done){
-        std::cout << "Error" << std::endl;
-        listener.close();
-        return 0;
+    while(player1.getRemoteAddress() == sf::IpAddress::None || player2.getRemoteAddress() == sf::IpAddress::None){
+        if(listener.listen(port) == sf::Socket::Done){
+            int ret = assign_player(player1, player2, listener);
+
+            if(ret != -1){
+                std::cout << "Player " << ret << " has connected! ";
+                std::cout << "Address: " << (ret == 1 ? player1 : player2).getRemoteAddress() << std::endl;
+            }
+        }
     }
-    // Wait for a connection
-    sf::TcpSocket socket;
-    if(listener.accept(socket) != sf::Socket::Done){
-        std::cout << "Error" << std::endl;
-        listener.close();
-        return 0;
-    }
-    std::cout << "New client connected: " << socket.getRemoteAddress() << std::endl;
-    
+
+    // if(listener.listen(port) != sf::Socket::Done){
+    //     std::cout << "Error" << std::endl;
+    //     listener.close();
+    //     return 0;
+    // }
+    // // Wait for a connection
+    // sf::TcpSocket socket;
+    // if(listener.accept(socket) != sf::Socket::Done){
+    //     std::cout << "Error" << std::endl;
+    //     listener.close();
+    //     return 0;
+    // }
+    // std::cout << "New client connected: " << socket.getRemoteAddress() << std::endl;
+
+
     // Receive a message from the client
     char buffer[1024];
 
     while(1){
         std::size_t received = 0;
-        socket.receive(buffer, sizeof(buffer), received);
-        if(received > 0){
-            std::cout << "The client said: " << buffer << std::endl;
+
+        // Player1
+        if(player1.receive(buffer, sizeof(buffer), received) == sf::Socket::Done){
+            std::cout << "Player1 said: " << buffer << std::endl;
             std::string response{"Hello world from the server!"};
-            socket.send(response.c_str(), response.size() + 1);
+            player1.send(response.c_str(), response.size() + 1);
+        }
+
+        // Player2
+        if(player2.receive(buffer, sizeof(buffer), received) == sf::Socket::Done){
+            std::cout << "Player2 said: " << buffer << std::endl;
+            std::string response{"Hello world from the server!"};
+            player2.send(response.c_str(), response.size() + 1);
         }
     }
     
+}
+
+
+int assign_player(sf::TcpSocket& player1, sf::TcpSocket& player2, sf::TcpListener& listener){
+    if(player1.getRemoteAddress() != sf::IpAddress::None){
+        if(listener.accept(player2) == sf::Socket::Done){
+            return 2;
+        }else{
+            return -1;
+        }
+    }else{
+        if(listener.accept(player1) == sf::Socket::Done){
+            return 1;
+        }else{
+            return -1;
+        }
+    }
 }
